5d541375605a1727f4154f80f2ae318056365f4d
Ferdy Rahmaesa <ferdy.suarial@bni.co.id>
2024-11-04 17:49:03 +0700

    add config retrofit additional

--- app/src/main/java/com/ferdsapp/moviefinder/core/network/config/ApiService.kt
+++ app/src/main/java/com/ferdsapp/moviefinder/core/network/config/ApiService.kt
@@ -4,14 +4,22 @@ import com.ferdsapp.moviefinder.core.network.model.nowPlaying.movie.ListMoviePla
 import com.ferdsapp.moviefinder.core.network.model.nowPlaying.tvShow.ListTvShowPlaying
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.QueryMap
 
 interface ApiService {
-    @Headers("Authorization")
-    @GET("3/movie/now_playing")
-    fun getMoviePlayingList(@QueryMap query: Map<String, String>) : Call<ListMoviePlaying>
 
-    @GET
-    fun getTvShowPlayingList(@QueryMap query: Map<String, String>): Call<ListTvShowPlaying>
+    @GET("3/movie/now_playing")
+    fun getMoviePlayingList(
+        @Header("Authorization") authToken: String,
+        @QueryMap query: Map<String, String>
+    ) : Call<ListMoviePlaying>
+
+
+    @GET("3/tv/airing_today")
+    fun getTvShowPlayingList(
+        @Header("Authorization") authToken: String,
+        @QueryMap query: Map<String, String>
+    ): Call<ListTvShowPlaying>
 }
